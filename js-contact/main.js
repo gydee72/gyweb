26b4a6594f6eae118882905af9ed9c7e4b43decf
Ragon Géraldine <gydee72@gmail.com>
2020-06-27 13:31:04 +0200

    Add files via upload

--- js-contact/main.js
+++ js-contact/main.js
@@ -2,9 +2,10 @@
 (function ($) {
     "use strict";
 
+
     /*==================================================================
     [ Focus Contact2 ]*/
-    $('.input100').each(function(){
+    $('.input3').each(function(){
         $(this).on('blur', function(){
             if($(this).val().trim() != "") {
                 $(this).addClass('has-val');
@@ -14,43 +15,59 @@
             }
         })    
     })
+            
 
     /*==================================================================
+    [ Chose Radio ]*/
+    $("#radio1").on('change', function(){
+        if ($(this).is(":checked")) {
+            $('.input3-select').slideUp(300);
+        }
+    });
+
+    $("#radio2").on('change', function(){
+        if ($(this).is(":checked")) {
+            $('.input3-select').slideDown(300);
+        }
+    });
+        
+  
+    
+    /*==================================================================
     [ Validate ]*/
-    var input = $('.validate-input .input100');
+    var name = $('.validate-input input[name="name"]');
+    var email = $('.validate-input input[name="email"]');
+    var message = $('.validate-input textarea[name="message"]');
+
 
     $('.validate-form').on('submit',function(){
         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
+        if($(name).val().trim() == ''){
+            showValidate(name);
+            check=false;
+        }
+
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        if($(message).val().trim() == ''){
+            showValidate(message);
+            check=false;
         }
 
         return check;
     });
 
 
-    $('.validate-form .input100').each(function(){
+    $('.validate-form .input3').each(function(){
         $(this).focus(function(){
            hideValidate(this);
-        });
+       });
     });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
 
     function showValidate(input) {
         var thisAlert = $(input).parent();
@@ -64,16 +81,6 @@
         $(thisAlert).removeClass('alert-validate');
     }
     
-
-    /*==================================================================
-    [ Show / hide Form ]*/
     
-    $('.contact100-btn-hide').on('click', function(){
-        $('.wrap-contact100').fadeOut(400);
-    })
-
-    $('.contact100-btn-show').on('click', function(){
-        $('.wrap-contact100').fadeIn(400);
-    })
 
 })(jQuery);
